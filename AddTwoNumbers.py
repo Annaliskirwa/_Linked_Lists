92eb6f341c9d1fec87db1e81dec6fe5f443ce8ab
Annaliskirwa <annaliskirwa@gmail.com>
2022-03-21 14:35:02 +0300

    Add Two Numbers

---
+++ AddTwoNumbers.py
@@ -0,0 +1,26 @@
+# You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+from typing import Optional
+from unittest import result
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2)-> Optional[ListNode]:
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = (l1.val if l1 else 0)
+            val2 = (l2.val if l2 else 0)
+            carry, out = divmod(val1+val2+carry, 10)
+
+            result_tail.next = ListNode(out)
+            result_tail =result_tail.next
+
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+
+        return result.next
+
+
